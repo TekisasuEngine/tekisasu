991257b6bcb6e56458932db78ca23b4452304a40
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2024-11-03 22:55:32 -0600

    header updates

--- editor/themes/editor_theme.h
+++ editor/themes/editor_theme.h
@@ -2,10 +2,11 @@
 /*  editor_theme.h                                                        */
 /**************************************************************************/
 /*                         This file is part of:                          */
-/*                             GODOT ENGINE                               */
-/*                        https://godotengine.org                         */
+/*                            TEKISASU ENGINE                             */
+/*                       https://dev.tekisasu.com                         */
 /**************************************************************************/
-/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2021-present Tekisasu (see AUTHORS.md).                  */
+/* Copyright (c) 2014-present Godot Engine contributors (see GODOT.md).   */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
 /*                                                                        */
 /* Permission is hereby granted, free of charge, to any person obtaining  */
