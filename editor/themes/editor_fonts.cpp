d5d68ef6c7405649a2388ff939499d07faca9d54
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2024-11-19 00:33:16 -0600

    [Blazium] Default UI font: enable Inter features

--- editor/themes/editor_fonts.cpp
+++ editor/themes/editor_fonts.cpp
@@ -209,6 +209,15 @@ void editor_register_fonts(const Ref<Theme> &p_theme) {
 	default_font_bold->set_fallbacks(fallbacks_bold);
 	default_font_bold_msdf->set_fallbacks(fallbacks_bold);
 
+	// Enable Inter OpenType features.
+	Dictionary ot_features;
+	ot_features["ss01"] = true; // Alternate digits (flat-top 3, open 4, 6 and 9)
+	ot_features["ss02"] = true; // Disambiguation (Upper-case I with serif, lower-case L with tail, slashed zero)
+	default_font->set_opentype_feature_overrides(ot_features);
+	default_font_msdf->set_opentype_feature_overrides(ot_features);
+	default_font_bold->set_opentype_feature_overrides(ot_features);
+	default_font_bold_msdf->set_opentype_feature_overrides(ot_features);
+
 	Ref<FontFile> default_font_mono = load_internal_font(_font_JetBrainsMono_Regular, _font_JetBrainsMono_Regular_size, font_mono_hinting, font_antialiasing, true, font_subpixel_positioning, font_disable_embedded_bitmaps);
 	default_font_mono->set_fallbacks(fallbacks);
 
