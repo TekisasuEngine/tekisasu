ce7beaecc638e238be370371e73ec32acc643a6a
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2024-12-09 03:28:20 -0600

    Theme updates
    - Remove the god awful light and Solarized light thmes
    - Rename Solarized (dark) to Solarized
    - Rename Tekisasu Engine to Tekisasu Dark
    - Add Tekisasu Indigo
    - Set default theme to Tekisasu Indigo

--- editor/themes/editor_theme_manager.cpp
+++ editor/themes/editor_theme_manager.cpp
@@ -291,14 +291,22 @@ EditorThemeManager::ThemeConfiguration EditorThemeManager::_create_theme_config(
 				preset_accent_color = Color(0.26, 0.76, 1.00);
 				preset_base_color = Color(0.24, 0.26, 0.28);
 				preset_contrast = config.default_contrast;
-			} else if (config.preset == "Tekisasu Engine") {
+			} else if (config.preset == "Tekisasu Dark") {
 				preset_accent_color = Color(0.40, 0.53, 0.91);
 				preset_base_color = Color(0.17, 0.17, 0.20);
+				preset_contrast = 0.4;
+			} else if (config.preset == "Tekisasu Indigo") {
+				preset_accent_color = Color(0.40, 0.52, 0.91);
+				preset_base_color = Color(0.21, 0.23, 0.36);
 				preset_contrast = 0.4;
 			} else if (config.preset == "Tekisasu Light") {
 				preset_accent_color = Color(0.49, 0.61, 0.93);
 				preset_base_color = Color(0.35, 0.37, 0.44);
 				preset_contrast = 0.4;
+			} else if (config.preset == "Breeze Dark") {
+				preset_accent_color = Color(0.26, 0.76, 1.00);
+				preset_base_color = Color(0.24, 0.26, 0.28);
+				preset_contrast = config.default_contrast;
 			} else if (config.preset == "Godot 2") {
 				preset_accent_color = Color(0.53, 0.67, 0.89);
 				preset_base_color = Color(0.24, 0.23, 0.27);
@@ -307,20 +315,10 @@ EditorThemeManager::ThemeConfiguration EditorThemeManager::_create_theme_config(
 				preset_accent_color = Color(0.44, 0.73, 0.98);
 				preset_base_color = Color(0.24, 0.24, 0.24);
 				preset_contrast = config.default_contrast;
-			} else if (config.preset == "Light") {
-				preset_accent_color = Color(0.18, 0.50, 1.00);
-				preset_base_color = Color(0.9, 0.9, 0.9);
-				// A negative contrast rate looks better for light themes, since it better follows the natural order of UI "elevation".
-				preset_contrast = -0.06;
-			} else if (config.preset == "Solarized (Dark)") {
+			} else if (config.preset == "Solarized") {
 				preset_accent_color = Color(0.15, 0.55, 0.82);
 				preset_base_color = Color(0.04, 0.23, 0.27);
 				preset_contrast = config.default_contrast;
-			} else if (config.preset == "Solarized (Light)") {
-				preset_accent_color = Color(0.15, 0.55, 0.82);
-				preset_base_color = Color(0.89, 0.86, 0.79);
-				// A negative contrast rate looks better for light themes, since it better follows the natural order of UI "elevation".
-				preset_contrast = -0.06;
 			} else if (config.preset == "Black (OLED)") {
 				preset_accent_color = Color(0.45, 0.75, 1.0);
 				preset_base_color = Color(0, 0, 0);
@@ -328,8 +326,8 @@ EditorThemeManager::ThemeConfiguration EditorThemeManager::_create_theme_config(
 				preset_contrast = 0.0;
 				preset_draw_extra_borders = true;
 			} else { // Default
-				preset_accent_color = Color(0.40, 0.53, 0.91);
-				preset_base_color = Color(0.17, 0.17, 0.20);
+				preset_accent_color = Color(0.40, 0.52, 0.91);
+				preset_base_color = Color(0.21, 0.23, 0.36);
 				preset_contrast = 0.4;
 			}
 
