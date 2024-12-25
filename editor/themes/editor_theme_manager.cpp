6a1fe4639ad142f9802b72cf5f98a596eddcdd38
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2024-12-25 00:10:08 -0600

    Increasing extra spacing to fix item list clipping arrows

--- editor/themes/editor_theme_manager.cpp
+++ editor/themes/editor_theme_manager.cpp
@@ -381,7 +381,7 @@ EditorThemeManager::ThemeConfiguration EditorThemeManager::_create_theme_config(
 				preset_dialogs_buttons_min_size = Size2(105, 34);
 			} else { // Default
 				preset_base_spacing = 1;
-				preset_extra_spacing = 1;
+				preset_extra_spacing = 2;
 				preset_dialogs_buttons_min_size = Size2(90, 24);
 			}
 
