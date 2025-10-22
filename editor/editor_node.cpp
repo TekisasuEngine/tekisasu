b9e2991699ed9ee0e131e0fa21441ecf65260719
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-10-22 14:21:09 -0500

    Disable 3D and AssetLib views by default

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -6526,10 +6526,10 @@ void EditorNode::_feature_profile_changed() {
 		editor_dock_manager->set_dock_enabled(NodeDock::get_singleton(), true);
 		editor_dock_manager->set_dock_enabled(FileSystemDock::get_singleton(), true);
 		editor_dock_manager->set_dock_enabled(history_dock, true);
-		main_editor_buttons[EDITOR_3D]->set_visible(true);
+		main_editor_buttons[EDITOR_3D]->set_visible(false);
 		main_editor_buttons[EDITOR_SCRIPT]->set_visible(true);
 		if (AssetLibraryEditorPlugin::is_available()) {
-			main_editor_buttons[EDITOR_ASSETLIB]->set_visible(true);
+			main_editor_buttons[EDITOR_ASSETLIB]->set_visible(false);
 		}
 	}
 }
