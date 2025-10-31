b9625fa714674d6fc1f1e6931373955c2fddf5e7
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-10-31 05:30:42 -0500

    Add tx rebase tools

---
+++ misc/scripts/rebase_headers.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+if [[ -z "$(which python)" ]]; then
+    echo "Python is not found in PATH. Exiting."
+    exit 0
+fi
+
+if [[ -z "$1" ]]; then
+    echo "no PATH to Tekisasu-Engine git directory provided in CLI argument."
+    echo " Usage:"
+    echo "  misc/scripts/rebase_headers.sh ROOT_DIR_OF_TEKISASU_ENGINE_GIT_REPO"
+    echo "  "
+    echo "Example:"
+    echo "  cd /mnt/c/Dev/git/tekisasu" 
+    echo "  misc/scripts/rebase_headers.sh ROOT_DIR_OF_TEKISASU_ENGINE_GIT_REPO"
+    echo " "
+    echo "Exiting."
+    echo " "
+    exit 0
+else
+    GIT_PATH="$1"
+fi
+
+if [[ ! -f "$GIT_PATH/version.py" ]]; then
+    echo "no valid Tekisasu Engine git repo path given."
+    echo " (version.py in directory specified not found)"
+    echo " "
+    echo "Exiting."
+    echo " "
+    exit 0
+fi
+
+HEADER_SCRIPT="$GIT_PATH/misc/scripts/copyright_headers.py"
+
+# *.mm files
+for folder in $(ls -d */ | grep -v pycache | grep -v thirdparty); do 
+    echo $folder
+    echo "---------------------------------------------"
+    for file in $(find $folder -type f -name "*.mm"); do 
+        echo "  $file"
+        $HEADER_SCRIPT $file
+    done
+    echo " "
+done
+
+# *.cpp files
+for folder in $(ls -d */ | grep -v pycache | grep -v thirdparty); do 
+    echo $folder
+    echo "---------------------------------------------"
+    for file in $(find $folder -type f -name "*.cpp"); do 
+        echo "  $file"
+        $HEADER_SCRIPT $file
+    done
+    echo " "
+done
+
+# *.h files
+for folder in $(ls -d */ | grep -v pycache | grep -v thirdparty); do 
+    echo $folder
+    echo "---------------------------------------------"
+    for file in $(find $folder -type f -name "*.h"); do 
+        echo "  $file"
+        $HEADER_SCRIPT $file
+    done
+    echo " "
+done
---
+++ misc/scripts/rebase_string_tool.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [[ -z "$(which python)" ]]; then
+    echo "Python is not found in PATH. Exiting."
+    exit 0
+fi
+
+if [[ -z "$1" ]]; then
+    echo "no PATH to Tekisasu-Engine git directory provided in CLI argument."
+    echo " Usage:"
+    echo "  misc/scripts/rebase_string_tool.sh ROOT_DIR_OF_TEKISASU_ENGINE_GIT_REPO OLD_STRING NEW_STRING"
+    echo "  "
+    echo "Example:"
+    echo "  cd /mnt/c/Dev/git/tekisasu" 
+    echo "  misc/scripts/rebase_string_tool.sh ROOT_DIR_OF_TEKISASU_ENGINE_GIT_REPO OLD_STRING NEW_STRING"
+    echo " "
+    echo "Exiting."
+    echo " "
+    exit 0
+else
+    GIT_PATH="$1"
+fi
+
+if [[ ! -f "$GIT_PATH/version.py" ]]; then
+    echo "no valid Tekisasu Engine git repo path given."
+    echo " (version.py in directory specified not found)"
+    echo " "
+    echo "Exiting."
+    echo " "
+    exit 0
+fi
+
+if [[ -z "$2" ]] && [[ -z "$3" ]]; then
+    echo "no valid old/new string replacement values given."
+    echo " Usage:"
+    echo "  misc/scripts/rebase_string_tool.sh ROOT_DIR_OF_TEKISASU_ENGINE_GIT_REPO OLD_STRING NEW_STRING"
+    echo "  "
+    echo "Example:"
+    echo "  cd /mnt/c/Dev/git/tekisasu" 
+    echo "  misc/scripts/rebase_string_tool.sh ROOT_DIR_OF_TEKISASU_ENGINE_GIT_REPO OLD_STRING NEW_STRING"
+    echo " "
+    echo "Exiting."
+    echo " "
+    exit 0
+fi
+
+OLD_STRING="$2"
+NEW_STRING="$3"
+
+echo "This will replace all instances of '$OLD_STRING' with '$NEW_STRING' in this Tekisasu Engine git repo"
+echo "on a case sensitive basis."
+echo " "
+echo "PROCEED? (y/n):"
+read value
+
+if [[ "$value" == "y" ]]; then
+    for file in $(grep -l -r $OLD_STRING . | grep -v "x86_64.obj" | grep -v "x86_64.lib" | grep -v pycache | grep -v "x86_64.exe"); do 
+        sed -i -e "s/$OLD_STRING/$NEW_STRING/g" "$file"
+        echo "processed: $file"
+    done
+elif [[ "$value" == "n" ]]; then
+    echo "Exiting."
+    echo " "
+    exit 0
+else
+    echo "invalid input. Exiting."
+    echo " "
+    exit 0
+fi
