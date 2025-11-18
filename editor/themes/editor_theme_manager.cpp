4491690b4c851fe10a6dd88e23e1fbffbc19ba9f
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-11-18 03:50:14 -0600

    New text editor default theme

--- editor/themes/editor_theme_manager.cpp
+++ editor/themes/editor_theme_manager.cpp
@@ -2463,18 +2463,18 @@ void EditorThemeManager::_generate_text_editor_defaults(ThemeConfiguration &p_co
 
 	/* clang-format off */
 	// Syntax highlight token colors.
-	const Color symbol_color =               p_config.dark_theme ? Color(0.67, 0.79, 1)      : Color(0, 0, 0.61);
-	const Color keyword_color =              p_config.dark_theme ? Color(1.0, 0.44, 0.52)    : Color(0.9, 0.135, 0.51);
-	const Color control_flow_keyword_color = p_config.dark_theme ? Color(1.0, 0.55, 0.8)     : Color(0.743, 0.12, 0.8);
-	const Color base_type_color =            p_config.dark_theme ? Color(0.26, 1.0, 0.76)    : Color(0, 0.6, 0.2);
-	const Color engine_type_color =          p_config.dark_theme ? Color(0.56, 1, 0.86)      : Color(0.11, 0.55, 0.4);
-	const Color user_type_color =            p_config.dark_theme ? Color(0.78, 1, 0.93)      : Color(0.18, 0.45, 0.4);
-	const Color comment_color =              p_config.dark_theme ? dim_color                 : Color(0.08, 0.08, 0.08, 0.5);
-	const Color doc_comment_color =          p_config.dark_theme ? Color(0.6, 0.7, 0.8, 0.8) : Color(0.15, 0.15, 0.4, 0.7);
-	const Color string_color =               p_config.dark_theme ? Color(1, 0.93, 0.63)      : Color(0.6, 0.42, 0);
+	const Color symbol_color =               p_config.dark_theme ? Color(0.67, 0.79, 1)          : Color(0, 0, 0.61);
+	const Color keyword_color =              p_config.dark_theme ? Color(1.0, 0.44, 0.52)        : Color(0.9, 0.135, 0.51);
+	const Color control_flow_keyword_color = p_config.dark_theme ? Color(1.0, 0.55, 0.8)         : Color(0.743, 0.12, 0.8);
+	const Color base_type_color =            p_config.dark_theme ? Color(0.26, 1.0, 0.76)        : Color(0, 0.6, 0.2);
+	const Color engine_type_color =          p_config.dark_theme ? Color(0.56, 1, 0.86)          : Color(0.11, 0.55, 0.4);
+	const Color user_type_color =            p_config.dark_theme ? Color(0.78, 1, 0.93)          : Color(0.18, 0.45, 0.4);
+	const Color comment_color =              p_config.dark_theme ? Color(0.20, 0.25, 0.52, 0.74) : Color(0.08, 0.08, 0.08, 0.5);
+	const Color doc_comment_color =          p_config.dark_theme ? Color(0.6, 0.7, 0.8, 0.8)	 : Color(0.15, 0.15, 0.4, 0.7);
+	const Color string_color =               p_config.dark_theme ? Color(1, 0.93, 0.63)          : Color(0.6, 0.42, 0);
 
 	// Use the brightest background color on a light theme (which generally uses a negative contrast rate).
-	const Color te_background_color =             p_config.dark_theme ? p_config.dark_color_2 : p_config.dark_color_3;
+	const Color te_background_color =             p_config.dark_theme ? Color(0.11, 0.11, 0.16, 1) : p_config.dark_color_3;
 	const Color completion_background_color =     p_config.dark_theme ? p_config.base_color : p_config.dark_color_2;
 	const Color completion_selected_color =       alpha1;
 	const Color completion_existing_color =       alpha2;
@@ -2483,8 +2483,8 @@ void EditorThemeManager::_generate_text_editor_defaults(ThemeConfiguration &p_co
 	const Color completion_scroll_hovered_color = Color(mono_value, mono_value, mono_value, 0.4);
 	const Color completion_font_color =           p_config.font_color;
 	const Color text_color =                      p_config.font_color;
-	const Color line_number_color =               dim_color;
-	const Color safe_line_number_color =          p_config.dark_theme ? (dim_color * Color(1, 1.2, 1, 1.5)) : Color(0, 0.4, 0, 0.75);
+	const Color line_number_color =               Color(0.24, 0.24, 0.25, 0.5);
+	const Color safe_line_number_color =          p_config.dark_theme ? (dim_color * Color(1, 1.2, 1, 1.5)) : Color(0.20, 0.35, 0.51, 0.74);
 	const Color caret_color =                     p_config.mono_color;
 	const Color caret_background_color =          p_config.mono_color.inverted();
 	const Color text_selected_color =             Color(0, 0, 0, 0);
