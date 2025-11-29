72315b9828e36e6496f2fd8b580e9922b85dc095
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-11-29 03:30:12 -0600

    Revert "Update editor_theme_manager.cpp"
    This reverts commit e4fc2a87bc9b3cc10a474fb641c0ddba0c4a9751.

--- editor/themes/editor_theme_manager.cpp
+++ editor/themes/editor_theme_manager.cpp
@@ -674,11 +674,10 @@ void EditorThemeManager::_create_shared_styles(const Ref<EditorTheme> &p_theme, 
 			}
 
 			p_config.button_style_pressed = p_config.button_style->duplicate();
-			p_config.button_style_pressed->set_bg_color(p_config.button_bg_color.lightened(0.125));
+			p_config.button_style_pressed->set_bg_color(p_config.button_bg_color.darkened(0.125));
 
 			p_config.button_style_hover = p_config.button_style->duplicate();
 			p_config.button_style_hover->set_bg_color(p_config.mono_color * Color(1, 1, 1, 0.11));
-			p_config.button_style_hover->set_bg_color(p_config.button_bg_color.darkened(0.125));
 			if (p_config.draw_extra_borders) {
 				p_config.button_style_hover->set_border_color(p_config.extra_border_color_1);
 			} else {
