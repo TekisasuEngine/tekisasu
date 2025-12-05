51e40dab77e7724a171a1b89557f38c7e4a7fe24
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-12-05 04:28:45 -0600

    Open scene drop down icon changed to FileList

--- editor/gui/editor_scene_tabs.cpp
+++ editor/gui/editor_scene_tabs.cpp
@@ -56,7 +56,7 @@ void EditorSceneTabs::_notification(int p_what) {
 			tabbar_panel->add_theme_style_override(SceneStringName(panel), get_theme_stylebox(SNAME("tabbar_background"), SNAME("TabContainer")));
 			scene_tabs->add_theme_constant_override("icon_max_width", get_theme_constant(SNAME("class_icon_size"), EditorStringName(Editor)));
 
-			scene_list->set_icon(get_editor_theme_icon(SNAME("GuiTabMenuHl")));
+			scene_list->set_icon(get_editor_theme_icon(SNAME("FileList")));
 
 			scene_tab_add->set_icon(get_editor_theme_icon(SNAME("Add")));
 			scene_tab_add->add_theme_color_override("icon_normal_color", Color(0.6f, 0.6f, 0.6f, 0.8f));
