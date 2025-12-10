9d6c036a06621292da08c48126d089220fb1cd01
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-12-10 13:01:52 -0600

    Reset external version numbers.
    version.py isn't utilized by Tekisasu builds.  tbuilder2 will generate version.gen.py from scratch with the appropriate release values.  Setting default version.py back to initial state for external version numbers.

--- version.py
+++ version.py
@@ -11,6 +11,6 @@ website = "https://dev.tekisasu.com"
 docs = "4.3"
 # External version: Tekisasu's public-facing version (used for user-visible UI and asset fetching)
 external_major = 1
-external_minor = 3
-external_patch = 1
-external_status = "stable"
+external_minor = 0
+external_patch = 0
+external_status = "dev"
