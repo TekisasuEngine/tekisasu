b457bed471c5042d4941f8c5e49bcf06ce13bc4b
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-12-10 12:59:18 -0600

    Reduce regular opacity of quick menu

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -7447,7 +7447,7 @@ EditorNode::EditorNode() {
 		title_bar->add_child(editor_logo_quick_menu);
 		editor_logo_quick_menu->set_tooltip_text(TTR("Tekisasu Engine"));
 		// Override icon color to use white instead of the default icon_normal_color
-		editor_logo_quick_menu->add_theme_color_override("icon_normal_color", Color(1, 1, 1, 0.8));
+		editor_logo_quick_menu->add_theme_color_override("icon_normal_color", Color(1, 1, 1, 0.75));
 		editor_logo_quick_menu->add_theme_color_override("icon_focus_color", Color(1, 1, 1, 0.9));
 		editor_logo_quick_menu->add_theme_color_override("icon_hover_color", Color(1, 1, 1, 0.9));
 		editor_logo_quick_menu->add_theme_color_override("icon_pressed_color", Color(1, 1, 1, 1));
