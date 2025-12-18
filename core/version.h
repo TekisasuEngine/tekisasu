d206494fd34b0c551d1a127135b765b68d5aee31
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-12-18 02:23:07 -0600

    Update version.h

--- core/version.h
+++ core/version.h
@@ -40,72 +40,72 @@
 #define _MKSTR(m_x) _STR(m_x)
 #endif
 
-// Godot versions are of the form <major>.<minor> for the initial release,
+// Tekisasu versions are of the form <major>.<minor> for the initial release,
 // and then <major>.<minor>.<patch> for subsequent bugfix releases where <patch> != 0
 // That's arbitrary, but we find it pretty and it's the current policy.
 
 // Defines the main "branch" version. Patch versions in this branch should be
 // forward-compatible.
 // Example: "3.1"
-#define GODOT_VERSION_BRANCH _MKSTR(TEKISASU_VERSION_MAJOR) "." _MKSTR(TEKISASU_VERSION_MINOR)
+#define TEKISASU_VERSION_BRANCH _MKSTR(TEKISASU_VERSION_MAJOR) "." _MKSTR(TEKISASU_VERSION_MINOR)
 #if TEKISASU_VERSION_PATCH
 // Example: "3.1.4"
-#define GODOT_VERSION_NUMBER GODOT_VERSION_BRANCH "." _MKSTR(TEKISASU_VERSION_PATCH)
+#define TEKISASU_VERSION_NUMBER TEKISASU_VERSION_BRANCH "." _MKSTR(TEKISASU_VERSION_PATCH)
 #else // patch is 0, we don't include it in the "pretty" version number.
 // Example: "3.1" instead of "3.1.0"
-#define GODOT_VERSION_NUMBER GODOT_VERSION_BRANCH
+#define TEKISASU_VERSION_NUMBER TEKISASU_VERSION_BRANCH
 #endif // TEKISASU_VERSION_PATCH
 
 // Version number encoded as hexadecimal int with one byte for each number,
 // for easy comparison from code.
 // Example: 3.1.4 will be 0x030104, making comparison easy from script.
-#define GODOT_VERSION_HEX 0x10000 * TEKISASU_VERSION_MAJOR + 0x100 * TEKISASU_VERSION_MINOR + TEKISASU_VERSION_PATCH
+#define TEKISASU_VERSION_HEX 0x10000 * TEKISASU_VERSION_MAJOR + 0x100 * TEKISASU_VERSION_MINOR + TEKISASU_VERSION_PATCH
 
-// Describes the full configuration of that Godot version, including the version number,
+// Describes the full configuration of that Tekisasu version, including the version number,
 // the status (beta, stable, etc.), potential module-specific features (e.g. mono)
 // and double-precision status.
 // Example: "3.1.4.stable.mono.double"
 #ifdef REAL_T_IS_DOUBLE
-#define GODOT_VERSION_FULL_CONFIG GODOT_VERSION_NUMBER "." TEKISASU_VERSION_STATUS GODOT_VERSION_MODULE_CONFIG ".double"
+#define TEKISASU_VERSION_FULL_CONFIG TEKISASU_VERSION_NUMBER "." TEKISASU_VERSION_STATUS TEKISASU_VERSION_MODULE_CONFIG ".double"
 #else
-#define GODOT_VERSION_FULL_CONFIG GODOT_VERSION_NUMBER "." TEKISASU_VERSION_STATUS GODOT_VERSION_MODULE_CONFIG
+#define TEKISASU_VERSION_FULL_CONFIG TEKISASU_VERSION_NUMBER "." TEKISASU_VERSION_STATUS TEKISASU_VERSION_MODULE_CONFIG
 #endif
 
-// Similar to GODOT_VERSION_FULL_CONFIG, but also includes the (potentially custom) TEKISASU_VERSION_BUILD
+// Similar to TEKISASU_VERSION_FULL_CONFIG, but also includes the (potentially custom) TEKISASU_VERSION_BUILD
 // description (e.g. official, custom_build, etc.).
 // Example: "3.1.4.stable.mono.double.official"
-#define GODOT_VERSION_FULL_BUILD GODOT_VERSION_FULL_CONFIG "." TEKISASU_VERSION_BUILD
+#define TEKISASU_VERSION_FULL_BUILD TEKISASU_VERSION_FULL_CONFIG "." TEKISASU_VERSION_BUILD
 
-// Same as above, but prepended with Godot's name and a cosmetic "v" for "version".
-// Example: "Godot v3.1.4.stable.official.mono.double"
-#define TEKISASU_VERSION_FULL_NAME GODOT_VERSION_NAME " v" GODOT_VERSION_FULL_BUILD
+// Same as above, but prepended with Tekisasu's name and a cosmetic "v" for "version".
+// Example: "Tekisasu v3.1.4.stable.official.mono.double"
+#define TEKISASU_VERSION_FULL_NAME TEKISASU_VERSION_NAME " v" TEKISASU_VERSION_FULL_BUILD
 
 // Git commit hash, generated at build time in `core/version_hash.gen.cpp`.
 extern const char *const TEKISASU_VERSION_HASH;
 
 // Git commit date UNIX timestamp (in seconds), generated at build time in `core/version_hash.gen.cpp`.
 // Set to 0 if unknown.
-extern const uint64_t GODOT_VERSION_TIMESTAMP;
+extern const uint64_t TEKISASU_VERSION_TIMESTAMP;
 
 #ifndef DISABLE_DEPRECATED
 // Compatibility with pre-4.5 modules.
-#define VERSION_SHORT_NAME GODOT_VERSION_SHORT_NAME
-#define VERSION_NAME GODOT_VERSION_NAME
+#define VERSION_SHORT_NAME TEKISASU_VERSION_SHORT_NAME
+#define VERSION_NAME TEKISASU_VERSION_NAME
 #define VERSION_MAJOR TEKISASU_VERSION_MAJOR
 #define VERSION_MINOR TEKISASU_VERSION_MINOR
 #define VERSION_PATCH TEKISASU_VERSION_PATCH
 #define VERSION_STATUS TEKISASU_VERSION_STATUS
 #define VERSION_BUILD TEKISASU_VERSION_BUILD
-#define VERSION_MODULE_CONFIG GODOT_VERSION_MODULE_CONFIG
-#define VERSION_WEBSITE GODOT_VERSION_WEBSITE
-#define VERSION_DOCS_BRANCH GODOT_VERSION_DOCS_BRANCH
-#define VERSION_DOCS_URL GODOT_VERSION_DOCS_URL
-#define VERSION_BRANCH GODOT_VERSION_BRANCH
-#define VERSION_NUMBER GODOT_VERSION_NUMBER
-#define VERSION_HEX GODOT_VERSION_HEX
-#define VERSION_FULL_CONFIG GODOT_VERSION_FULL_CONFIG
-#define VERSION_FULL_BUILD GODOT_VERSION_FULL_BUILD
+#define VERSION_MODULE_CONFIG TEKISASU_VERSION_MODULE_CONFIG
+#define VERSION_WEBSITE TEKISASU_VERSION_WEBSITE
+#define VERSION_DOCS_BRANCH TEKISASU_VERSION_DOCS_BRANCH
+#define VERSION_DOCS_URL TEKISASU_VERSION_DOCS_URL
+#define VERSION_BRANCH TEKISASU_VERSION_BRANCH
+#define VERSION_NUMBER TEKISASU_VERSION_NUMBER
+#define VERSION_HEX TEKISASU_VERSION_HEX
+#define VERSION_FULL_CONFIG TEKISASU_VERSION_FULL_CONFIG
+#define VERSION_FULL_BUILD TEKISASU_VERSION_FULL_BUILD
 #define VERSION_FULL_NAME TEKISASU_VERSION_FULL_NAME
 #define VERSION_HASH TEKISASU_VERSION_HASH
-#define VERSION_TIMESTAMP GODOT_VERSION_TIMESTAMP
+#define VERSION_TIMESTAMP TEKISASU_VERSION_TIMESTAMP
 #endif // DISABLE_DEPRECATED
