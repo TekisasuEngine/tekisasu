bbaf9f6e51349543a964cedd3f9a84f2ddcf1141
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-12-17 22:23:36 -0600

    Enable Inter OpenType features

--- editor/themes/editor_fonts.cpp
+++ editor/themes/editor_fonts.cpp
@@ -194,6 +194,15 @@ void editor_register_fonts(const Ref<Theme> &p_theme) {
 	default_font->set_fallbacks(fallbacks);
 	default_font_msdf->set_fallbacks(fallbacks);
 
+	// Enable Inter OpenType features.
+	Dictionary ot_features;
+	ot_features["ss01"] = true; // Alternate digits (flat-top 3, open 4, 6 and 9)
+	ot_features["ss02"] = false; // Disambiguation (Upper-case I with serif, lower-case L with tail, slashed zero)
+	default_font->set_opentype_feature_overrides(ot_features);
+	default_font_msdf->set_opentype_feature_overrides(ot_features);
+	default_font_bold->set_opentype_feature_overrides(ot_features);
+	default_font_bold_msdf->set_opentype_feature_overrides(ot_features);
+
 	Ref<FontFile> default_font_bold = load_internal_font(_font_Inter_Bold, _font_Inter_Bold_size, font_hinting, font_antialiasing, true, font_subpixel_positioning, font_disable_embedded_bitmaps, false);
 	Ref<FontFile> default_font_bold_msdf = load_internal_font(_font_Inter_Bold, _font_Inter_Bold_size, font_hinting, font_antialiasing, true, font_subpixel_positioning, font_disable_embedded_bitmaps, font_allow_msdf);
 
